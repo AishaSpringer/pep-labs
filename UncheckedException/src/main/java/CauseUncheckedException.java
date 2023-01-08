cc4e4968df2cdd742e756fafb607e2a96beb615b
Aisha <aishaspringer@gmail.com>
2023-01-08 22:20:47 +0000

    Saved progress on UncheckedException 2023-01-08 22:20:47 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,7 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int b = 3, c = 0;
+        int a = b / c;
     }
 }
