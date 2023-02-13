be8b8345d11078ca1c5dd946082d1d8a8954f5da
Aisha <aishaspringer@gmail.com>
2023-02-13 01:08:56 +0000

    Saved progress on AdvancedBooleans 2023-02-13 01:08:56 +0000

--- AdvancedBooleans/src/main/java/AdvancedBooleanLogic.java
+++ AdvancedBooleans/src/main/java/AdvancedBooleanLogic.java
@@ -16,8 +16,12 @@ public class AdvancedBooleanLogic {
      * @return false if b1 and b2 are both true. Otherwise, return true.
      */
     public boolean returnNotBoth(boolean b1, boolean b2){
-        return false;
+        if (b1 == true && b2 == true){
+            return false;
+        } else {
+        return true;
     }
+}
     /**
      * b1 OR b2, BUT NOT BOTH
      * @param b1 first boolean to be compared.
@@ -25,9 +29,15 @@ public class AdvancedBooleanLogic {
      * @return true if either b1 or b2 is true, but false if they are both true or both false.
      */
     public boolean returnExclusiveOr(boolean b1, boolean b2){
-        return false;
-    }
-
+        if ((b1 && !b2) || (!b1 && b2)) {
+            return true;
+        } else {
+            if ((b1 && b2) || !(b1 && b2)) {
+                
+            }
+            return false;
+    } 
+}
     /**
      * Neither b1 NOR b2
      * @param b1 first boolean to be compared.
@@ -35,6 +45,13 @@ public class AdvancedBooleanLogic {
      * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
      */
     public boolean returnNeitherNor(boolean b1, boolean b2){
-        return false;
+        if (b1 || b2 == true) {
+            return false;
+        } else {
+            if (b1 && b2 == false) {
+                
+            }
+            return true;
+        }
     }
 }
